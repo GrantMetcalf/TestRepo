b64b3e34ac0e06fe96fe0d727bd5067da0044bca
Your Name <you@example.com>
2020-02-26 00:00:23 +0200

    Added circumference calculation

--- TestRepo/src/main/java/org/example/App.java
+++ TestRepo/src/main/java/org/example/App.java
@@ -10,7 +10,7 @@ public class App
 
     {
         System.out.println( "We will now calculate the area of a 2d square or rectangle" );
-        int length, breadth, area;
+        int length, breadth, area, circumference;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the length please : ");
         length = sc.nextInt();
@@ -19,6 +19,8 @@ public class App
 
         area = length * breadth;
         System.out.println("The area of the shape is: " + area);
+        circumference = (length * 2) + (breadth * 2);
+        System.out.println("The circumference of the shape is: " + circumference);
     }
 
 }
